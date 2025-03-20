eca3cc372cc3bb3aa8e79a277346dce51d6bb3ed
Amanda Anderson-You <a.moeller.anderson@gmail.com>
2025-03-20 13:29:09 -0500

    add and connect elevation cog

--- script.js
+++ script.js
@@ -12,26 +12,26 @@ var map = L.map('map').setView([33.8734, -115.9010], 10);
 
 
 
-// // load geotiff layer
-// fetch('../vegetation_cogs/foo.tif')
+// load geotiff layer
+fetch('../elevation_cog/Copernicus_DSM_COG_10_N33_00_W117_00_DEM.tif')
 
-// // fetch('../output_hh.tif')
-// .then(response => response.arrayBuffer())
-// .then(arrayBuffer => {
-//     parseGeoraster(arrayBuffer).then(georaster => {
-//     console.log("georaster:", georaster);
+// fetch('../output_hh.tif')
+.then(response => response.arrayBuffer())
+.then(arrayBuffer => {
+    parseGeoraster(arrayBuffer).then(georaster => {
+    console.log("georaster:", georaster);
 
-// var layer = new GeoRasterLayer({
-//     georaster: georaster,
-//     opacity: .7,
-//     // adjust value and color scale for success, density, survivability?
-//     pixelValuesToColorFn: values => values[0] === 42 ? '#ffffff' : '#000000',
-//     resolution: 64 // optional parameter for adjusting display resolution
-// });
+var layer = new GeoRasterLayer({
+    georaster: georaster,
+    opacity: .7,
+    // adjust value and color scale for success, density, survivability?
+    // pixelValuesToColorFn: values => values[0] === 42 ? '#ffffff' : '#000000',
+    resolution: 64 // optional parameter for adjusting display resolution
+});
 
-// layer.addTo(map);
+layer.addTo(map);
 
-// map.fitBounds(layer.getBounds());
+map.fitBounds(layer.getBounds());
 
-// });
-// });
+});
+});
