90c03f58c44c93a964224f4765b15899377ad52d
Amanda Anderson-You <a.moeller.anderson@gmail.com>
2025-03-21 12:57:30 -0500

    add css, define colors

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 // script code to load map with georaster layer
 
-// map initialization
-var map = L.map('map').setView([33.8734, -115.9010], 10);
 
-// open street map layer
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// Function to get CSS variable value
+function getCSSVariableValue(variableName) {
+    return getComputedStyle(document.documentElement).getPropertyValue(variableName).trim();
+  }
+  
+  // map initialization
+  const map = L.map('map').setView([33.8734, -115.9010], 10);
+  
+  const primary = getCSSVariableValue('--primary-color');
+  const secondary = getCSSVariableValue('--secondary-color');
+  const tertiary = getCSSVariableValue('--tertiary-color');
+  const quaternary = getCSSVariableValue('--quaternary-color');
+  const quinary = getCSSVariableValue('--quinary-color');
+  
+  // OpenStreetMap layer
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-
+  }).addTo(map);
 
 
 // load geotiff layer
@@ -25,7 +35,23 @@ var layer = new GeoRasterLayer({
     georaster: georaster,
     opacity: .7,
     // adjust value and color scale for success, density, survivability?
-    // pixelValuesToColorFn: values => values[0] === 42 ? '#ffffff' : '#000000',
+    pixelValuesToColorFn: function (value) {
+        if (value < 100) {
+            return rgb(55, 8, 85);
+        } else if (value > 100 && value < 500) {
+            return rgb(39, 80, 123);
+        } else if (value > 500 && value < 1000) {
+            return rgb(31, 120, 122);
+        } else if (value > 1000 && value < 1500) {
+            return rgb(52, 178, 98);
+        } else if (value > 1500) {
+            return rgb(171, 219, 32);
+        } else {
+            return "transparent"
+        }
+    },
+
+    },
     resolution: 64 // optional parameter for adjusting display resolution
 });
 
