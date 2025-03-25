486db9e0a6e79bfe3b4daf70bcdf2d8a67c8e118
Amanda Anderson-You <a.moeller.anderson@gmail.com>
2025-03-25 16:05:08 -0500

    created two script files for loading cog two ways, see readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # fire-recovery-frontend
 Front end and visualization code for DSE fire recovery project
 
+script.js file loads from URL while scriptAlt.js loads from a locally saved COG file.
--- scriptAlt.js
+++ scriptAlt.js
@@ -1,99 +1,65 @@
-// Alt version for loading COG using geotiff.js
+// script code to load map with georaster layer from a locally saved file
 
-
-
-// ***************DOES NOT CURRENTLY WORK! JUST EXPERIMENTING.***************
-
-
-// import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
-
-const url = 'https://elevationeuwest.blob.core.windows.net/copernicus-dem/COP30_hh/Copernicus_DSM_COG_10_N33_00_W117_00_DEM.tif?st=2025-03-20T20%3A05%3A29Z&se=2025-03-21T20%3A50%3A29Z&sp=rl&sv=2024-05-04&sr=c&skoid=9c8ff44a-6a2c-4dfb-b298-1c9212f64d9a&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2025-03-21T20%3A03%3A27Z&ske=2025-03-28T20%3A03%3A27Z&sks=b&skv=2024-05-04&sig=yb/QDMfmuGK%2BTzoxeUzh18nLGdtGHuzoncXRmA0gWX4%3D';
-
-//load tiff using async
-async function loadTIFF(url) {
-    try {
-      const tiff = await GeoTIFF.fromUrl(url);
-      const image = await tiff.getImage();
-      
-      // Getting raster data
-      const rasters = await image.readRasters();
-      console.log(rasters);
-      
-    } catch (error) {
-      console.error("Error loading the TIFF:", error);
-    }
+// Function to get CSS variable value
+function getCSSVariableValue(variableName) {
+  return getComputedStyle(document.documentElement).getPropertyValue(variableName).trim();
   }
   
-
-// Convert the data into geojson for using in D3 and Leaflet
-function processData(data) {
-    // Example: Convert raster data to GeoJSON points
-    const geojson = {
-      type: "FeatureCollection",
-      features: []
-    };
+  // URL for the COG
+  // const url = 'https://elevationeuwest.blob.core.windows.net/copernicus-dem/COP30_hh/Copernicus_DSM_COG_10_N33_00_W117_00_DEM.tif?st=2025-03-20T20%3A05%3A29Z&se=2025-03-21T20%3A50%3A29Z&sp=rl&sv=2024-05-04&sr=c&skoid=9c8ff44a-6a2c-4dfb-b298-1c9212f64d9a&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2025-03-21T20%3A03%3A27Z&ske=2025-03-28T20%3A03%3A27Z&sks=b&skv=2024-05-04&sig=yb/QDMfmuGK%2BTzoxeUzh18nLGdtGHuzoncXRmA0gWX4%3D';
   
-    for (let i = 0; i < data.width; i++) {
-      for (let j = 0; j < data.height; j++) {
-        const value = data[0][i * data.width + j];
-        if (value) {
-          geojson.features.push({
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: [-116.065750, 33.82251] // Adjust coordinates as needed
-            },
-            properties: {
-              value: value
-            }
-          });
-        }
-      }
-    }
-  
-    return geojson;
-  }
-
-  // make markers in D3
-  
-  function addMarkersToMap(geojson, map) {
-    const svg = d3.select(map.getPanes().overlayPane).append("svg");
-    const g = svg.append("g").attr("class", "leaflet-zoom-hide");
-  
-    const transform = d3.geoTransform({ point: projectPoint });
-    const path = d3.geoPath().projection(transform);
-  
-    const feature = g.selectAll("path")
-      .data(geojson.features)
-      .enter().append("path");
-  
-    map.on("viewreset", reset);
-    reset();
-  
-    function reset() {
-      const bounds = path.bounds(geojson);
-      const topLeft = bounds[0];
-      const bottomRight = bounds[1];
-  
-      svg.attr("width", bottomRight[0] - topLeft[0])
-         .attr("height", bottomRight[1] - topLeft[1])
-         .style("left", topLeft[0] + "px")
-         .style("top", topLeft[1] + "px");
-  
-      g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
-  
-      feature.attr("d", path);
-    }
-  
-    function projectPoint(x, y) {
-      const point = map.latLngToLayerPoint(new L.LatLng(y, x));
-      this.stream.point(point.x, point.y);
-    }
-  }
-
-  // load COG file and process the data and add markers to map
+  // map initialization
   const map = L.map('map').setView([33.8734, -115.9010], 10);
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
   
- 
+  // OpenStreetMap layer
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(map);
   
+  // set colors
+  const primary = getCSSVariableValue('--primary-color');
+  const secondary = getCSSVariableValue('--secondary-color');
+  const tertiary = getCSSVariableValue('--tertiary-color');
+  const quaternary = getCSSVariableValue('--quaternary-color');
+  const quinary = getCSSVariableValue('--quinary-color');
+  
+  
+  // import georaster from 'georaster';
+
+  
+  // ****************** USE BELOW FOR LOADING FROM LOCAL ******************
+  fetch('../elevation_cog/Copernicus_DSM_COG_10_N33_00_W117_00_DEM.tif')
+  .then(response => response.arrayBuffer())
+  .then(arrayBuffer => {
+      parseGeoraster(arrayBuffer).then(georaster => {
+      console.log("georaster:", georaster);
+  
+  
+      
+      var layer = new GeoRasterLayer({
+          georaster: georaster,
+          opacity: 0.5,
+          // adjust value and color scale for success, density, survivability?
+          pixelValuesToColorFn: function (value) {
+          if (value < 100) {
+              return primary;
+          } else if (value > 100 && value < 500) {
+              return secondary;
+          } else if (value > 500 && value < 1000) {
+              return tertiary;
+          } else if (value > 1000 && value < 1500) {
+              return quaternary;
+          } else if (value > 1500) {
+              return quinary;
+          } else {
+              return "transparent";
+          }
+          },
+          resolution: 64 // optional parameter for adjusting display resolution
+      });
+  
+      layer.addTo(map);
+      map.fitBounds(layer.getBounds());
+      });
+  });
