0fa9fecd34cfb4b8e4828c2c3977e7e9541058f1
Amanda Anderson-You <a.moeller.anderson@gmail.com>
2025-03-31 12:04:42 -0500

    header images

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//placeholder script file for home page
+// Load images at random from a local folder.
+window.onload = function() {
+    const images = [
+        'arches.jpg',
+        'bryceCanyon.jpg',
+        'glacier.jpg',
+        'grandTeton.jpg',
+        'joshuaTree.jpg',
+        'joshuaTree2.jpg',
+        'rockyMountains.jpg',
+        'yellowstone.jpg',
+        'yosemite.jpg',
+    ];
+
+    const getRandomImage = () => {
+        const randomIndex = Math.floor(Math.random() * images.length);
+        return images[randomIndex];
+    };
+
+    const setHeroImage = () => {
+        const heroImageElement = document.querySelector('.hero-image');
+        const imageUrl = `headerImages/${getRandomImage()}`;
+
+        heroImageElement.style.backgroundImage = `url(${imageUrl})`;
+    };
+
+    setHeroImage();
+
+    // Optional: Add logic to determine which page you're on
+    // For demonstration purposes, assume home if URL ends with "index.html"
+    if (window.location.pathname.endsWith('index.html')) {
+        document.body.classList.add('home');
+    } else {
+        document.body.classList.add('page');
+    }
+};
