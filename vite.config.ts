e49ef3d75f31a399ebda02b117aa3b4aea613de6
GondekNP <npg@berkeley.edu>
2025-12-09 23:46:23 +0000

    vite config for proper src location

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
+  root: 'src',  // HTML files are in src/
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
